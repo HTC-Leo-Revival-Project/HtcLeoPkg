a276569fa61c0f6f177bb621700142967afe77c6
Aljoshua Hell <aljoshua.hell@gmail.com>
2026-03-22 02:10:32 +0100

    QSD8250PKG: add support for HtcPassion
    add support for Htc Nexus one ... again... for the 5th time ....

--- QSD8250Pkg/Library/QSD8250PkgLib/QSD8250PkgMem.c
+++ QSD8250Pkg/Library/QSD8250PkgLib/QSD8250PkgMem.c
@@ -43,6 +43,9 @@ STATIC struct ReservedMemory {
     { 0x00400000, 0x02100000 },    // AMSS
     { 0x10000000, 0x01800000 },    // QDSP6
     { 0x2FFC0000, 0x00040000 },    // PSTORE
+#if ENABLEUART == 1
+    { 0xA9A00000, 0x00001000 },    //UART
+#endif
     
     { FB_ADDR,    FB_SIZE    },    // Display Reserved
 };
